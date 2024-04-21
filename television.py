182516daac469b75a268eee11a44bddb6b00613b
Hiromi Nurse <90805655+AriesTheDesty@users.noreply.github.com>
2024-04-21 13:49:35 -0500

    Updated television.py

--- television.py
+++ television.py
@@ -9,17 +9,15 @@ class Television:
         Function Initialize instance variables
         :param status: Boolean power state
         :param muted: Boolean mute state
-        :return: none
         """
         self.__status = status
         self.__muted = muted
-        self.__volume = self.MIN_VOLUME
-        self.__channel = self.MIN_CHANNEL
+        self.__volume = Television.MIN_VOLUME
+        self.__channel = Television.MIN_CHANNEL
 
     def power(self) -> None:
         """
         Function toggle status
-        :return:
         """
         if self.__status:
             self.__status = False
@@ -29,7 +27,6 @@ class Television:
     def mute(self) -> None:
         """
         Function toggle mute
-        :return: none
         """
         if self.__status:
             if self.__muted:
@@ -40,48 +37,48 @@ class Television:
     def channel_up(self) -> None:
         """
         Function increase channel by 1 or loop minimum
-        :return: none
         """
         if self.__status:
-            if self.__channel < self.MAX_CHANNEL:
+            if self.__channel < Television.MAX_CHANNEL:
                 self.__channel += 1
             else:
-                self.__channel = self.MIN_CHANNEL
+                self.__channel = Television.MIN_CHANNEL
 
     def channel_down(self) -> None:
         """
         Function decrease channel by 1 or loop to maximum
-        :return: none
         """
         if self.__status:
-            if self.__channel > self.MIN_CHANNEL:
+            if self.__channel > Television.MIN_CHANNEL:
                 self.__channel -= 1
             else:
-                self.__channel = self.MAX_CHANNEL
+                self.__channel = Television.MAX_CHANNEL
 
     def volume_up(self) -> None:
         """
         Function increase volume by 1 unless at maximum
-        :return: none
         """
         if self.__status:
             if self.__muted:
                 self.__muted = False
-            if self.__volume < self.MAX_VOLUME:
+            if self.__volume < Television.MAX_VOLUME:
                 self.__volume += 1
 
     def volume_down(self) -> None:
         """
         Function decrease volume by 1 unless at minimum
-        :return: none
         """
         if self.__status:
             if self.__muted:
                 self.__muted = False
-            if self.__volume > self.MIN_VOLUME:
+            if self.__volume > Television.MIN_VOLUME:
                 self.__volume -= 1
 
     def __str__(self) -> str:
+        """
+        Function formats the value returned with a television object is called as a string
+        :return: A String formated to describe power status, channel, and volume
+        """
         if self.__muted:
             return f"Power = {self.__status}, Channel = {self.__channel}, Volume = 0"
         else:
